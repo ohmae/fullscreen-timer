66d1a8d7560d15aa5bfc9f674a738d2eb5410ce9
OHMAE Ryosuke <ryo@mm2d.net>
2023-04-15 08:55:49 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.7.0")
     implementation("androidx.browser:browser:1.5.0")
