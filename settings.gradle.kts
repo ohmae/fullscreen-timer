aaf1a4a9a12d3cd80d852e2bf0982d4137747a9a
OHMAE Ryosuke <ryo@mm2d.net>
2023-05-07 20:03:57 +0900

    🆙AGP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,8 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
 rootProject.name = "fullscreen-timer"
 include(":app")
