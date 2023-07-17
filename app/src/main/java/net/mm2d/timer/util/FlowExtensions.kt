15ee376855aa9d0f89b3699a1fb240f181a095eb
OHMAE Ryosuke <ryo@mm2d.net>
2023-07-17 11:20:58 +0900

    ✨observeOnce

--- app/src/main/java/net/mm2d/timer/util/FlowExtensions.kt
+++ app/src/main/java/net/mm2d/timer/util/FlowExtensions.kt
@@ -4,11 +4,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 fun <T> Flow<T>.observe(
@@ -17,19 +15,11 @@ fun <T> Flow<T>.observe(
     action: suspend (T) -> Unit,
 ) = flowWithLifecycle(owner.lifecycle, minActiveState)
     .distinctUntilChanged()
-    .onEach(action)
-    .launchIn(owner.lifecycleScope)
+    .apply { owner.lifecycleScope.launch { collect { action(it) } } }
 
 fun <T> Flow<T>.observeOnce(
     owner: LifecycleOwner,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
     action: suspend (T) -> Unit,
 ) = flowWithLifecycle(owner.lifecycle, minActiveState)
-    .apply {
-        owner.lifecycleScope.launch {
-            collect {
-                action(it)
-                cancel()
-            }
-        }
-    }
+    .apply { owner.lifecycleScope.launch { action(first()) } }
