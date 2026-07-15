ac976f1d6a066b977c6fcc506be5129c905defda
OHMAE Ryosuke <ryo@mm2d.net>
2026-07-15 20:30:54 +0900

    🎉release 0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 
 val applicationName = "Timer"
 val versionMajor = 0
-val versionMinor = 11
-val versionPatch = 8
+val versionMinor = 12
+val versionPatch = 0
 
 android {
     namespace = "net.mm2d.timer"
